3c048acc6da8af9309401b78e22d404d8c7e32b5
Lacwil <lachlan@yoursoundtrak.com>
2025-04-30 12:03:24 +1000

    Chore: Update Vite proxy config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// 👇 This enables API proxying in dev and allows Vercel to handle API routes
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
+  }
 });
